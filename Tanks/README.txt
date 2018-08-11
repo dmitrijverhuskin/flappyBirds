6e941dae2d8c782c1e33451724dc6cce48d1ac66
dmitrijverhushkin <dmitrijverhuskin@gmail.com>
2018-08-11 20:37:41 +0300

    eted:    ../Tanks/README.txt eted:    ../Tanks/build.xml eted:    ../Tanks/build/classes/.netbeans_automatic_build eted:    ../Tanks/build/classes/.netbeans_update_resources eted:    ../Tanks/build/classes/graphics/TecstureAtlas.class eted:    ../Tanks/build/classes/tanks/Display$1.class eted:    ../Tanks/build/classes/tanks/com/thebuteguru/display/Display.class eted:    ../Tanks/build/classes/tanks/com/thebuteguru/display/Display.rs eted:    ../Tanks/build/classes/tanks/com/thebuteguru/display/Input$1.class eted:    ../Tanks/build/classes/tanks/com/thebuteguru/display/Input$2.class eted:    ../Tanks/build/classes/tanks/com/thebuteguru/display/Input.class eted:    ../Tanks/build/classes/tanks/com/thebuteguru/display/game/Game.class eted:    ../Tanks/build/classes/tanks/com/thebuteguru/display/utils/ResourseLouder.class eted:    ../Tanks/build/classes/tanks/com/thebuteguru/display/utils/Time.class eted:    ../Tanks/build/classes/tanks/com/thebyteguru/main/main.class eted:    ../Tanks/manifest.mf eted:    ../Tanks/nbproject/build-impl.xml eted:    ../Tanks/nbproject/genfiles.properties eted:    ../Tanks/nbproject/private/private.properties eted:    ../Tanks/nbproject/private/private.xml eted:    ../Tanks/nbproject/project.properties eted:    ../Tanks/nbproject/project.xml eted:    ../Tanks/res/texture_atlas.png eted:    ../Tanks/src/graphics/TecstureAtlas.java eted:    ../Tanks/src/tanks/com/thebuteguru/display/Display.java eted:    ../Tanks/src/tanks/com/thebuteguru/display/IO/Input.java eted:    ../Tanks/src/tanks/com/thebuteguru/display/game/Game.java eted:    ../Tanks/src/tanks/com/thebuteguru/display/utils/ResourseLouder.java eted:    ../Tanks/src/tanks/com/thebuteguru/display/utils/Time.java eted:    ../Tanks/src/tanks/com/thebyteguru/main/main.java

--- Tanks/README.txt
+++
@@ -1,1 +0,0 @@
-Console game on Java 'Clone Tanks'
--- Tanks/src/tanks/com/thebuteguru/display/utils/Time.java
+++
@@ -1,9 +0,0 @@
-package tanks.com.thebuteguru.display.utils;
-
-public class Time {
-    public static final long SECOND = 1000000000l;
-    
-    public static long get(){
-        return System.nanoTime();
-    }
-}
